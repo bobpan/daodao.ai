50aa9df2f907ab6d69ee9f3c7d7222c4324a03e9
小小的天 <bobpan@users.noreply.github.com>
2023-02-27 17:14:14 +0800

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,9 +3,15 @@
 export default function handler(req, res) {
   //res.status(200).json({ outputArguments: req.query.inputArguments })
   res.status(200).json({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies,
-    outputArguments: req.query.inputArguments,
+    "err_code": 0,                // 状态码，可在获得响应后区分下一步操作
+    "data_list": [
+      {                           // 目前返回信息需要包裹在 `data_list` 中唯一对象内
+        body: req.body,
+        query: req.query,
+        cookies: req.cookies,
+        outputArguments: req.query.inputArguments,
+        encryptText: req.query.encryptText,
+      }
+    ]
   });
 }
