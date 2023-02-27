7e374bbab8884dbdd944cd83a22518de52fc7f39
小小的天 <bobpan@users.noreply.github.com>
2023-02-27 17:32:35 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/api/:slug',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
