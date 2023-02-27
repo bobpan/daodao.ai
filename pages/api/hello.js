a8f3122d4e4b7830266ac68b358ca123d699fcf8
小小的天 <bobpan@users.noreply.github.com>
2023-02-27 17:08:27 +0800

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  //res.status(200).json({ outputArguments: req.query.inputArguments })
+  res.status(200).json({
+    body: req.body,
+    query: req.query,
+    cookies: req.cookies,
+    outputArguments: req.query.inputArguments,
+  });
 }
