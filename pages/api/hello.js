4779b93bb23e53f190f1eaf93733e89cc5baa4ca
小小的天 <bobpan@users.noreply.github.com>
2023-02-28 14:05:00 +0800

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,13 +3,14 @@
 export default function handler(req, res) {
   //res.status(200).json({ outputArguments: req.query.inputArguments })
   res.setHeader('Content-Type', 'application/json')
+  res.setHeader('charset', 'utf-8')
   res.status(200).json({
     "err_code": 0,                // 状态码，可在获得响应后区分下一步操作
     "data_list": [
       {                           // 目前返回信息需要包裹在 `data_list` 中唯一对象内
         body: req.body,
         query: req.query,
-        cookies: req.cookies,
+        //cookies: req.cookies,
         outputArguments: req.query.inputArguments,
         encryptText: req.query.encryptText,
       }
